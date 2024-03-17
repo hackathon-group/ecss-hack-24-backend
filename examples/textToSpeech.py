acc86b067d17d101f08d04b80a1b7e5530eaf03f
anthi <anthi2002@hotmail.com>
2024-03-17 09:22:35 +0000

    added methods

--- examples/textToSpeech.py
+++ examples/textToSpeech.py
@@ -1,24 +1,30 @@
 from pathlib import Path
 from openai import OpenAI
-import os
 
 apiKey = "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"
-
-#headers = {"Authorization": f"Bearer {apiKey}",
-            #"Content-Type": "application/json"}
 
 speech_file_path = Path(__file__).parent / "speech.mp3"
 
 client = OpenAI(api_key=apiKey)
 
-#openai.api_key = "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"
-#default_headers = headers
+text = "Today is a wonderful day to build something people love!"
 
 response = client.audio.speech.create(
   model="tts-1",
   voice="alloy",
-  input="Today is a wonderful day to build something people love!"
+  input=text
 )
 
 with open(speech_file_path, "wb") as f:
     f.write(response.content)
+
+
+def text2speech(text):
+
+  response = client.audio.speech.create(
+    model="tts-1",
+    voice="alloy",
+    input=text
+  )
+
+  return(response.content)
--- src/speech_text.py
+++ src/speech_text.py
@@ -1,3 +1,5 @@
+import requests
+import io
 from openai import OpenAI
 
 APIKEY = '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'
@@ -11,3 +13,18 @@ transcription = client.audio.transcriptions.create(
 )
 print(transcription)
 
+def speech2text(audio_url):
+
+    response = requests.get(audio_url)
+    if response.status_code == 200:
+
+        audio_file = io.BytesIO(response.content)
+        
+        transcription = client.audio.transcriptions.create(
+        model="whisper-1", 
+        file=audio_file, 
+        response_format="text"
+        )
+        return(transcription)
+    else:
+        return("Failed to download the file from the URL.")
